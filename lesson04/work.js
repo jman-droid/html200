c84d10ab43dc159b1144f9f182fe576d554a2e8c
code personn (beginner level) <63759439+jman-droid@users.noreply.github.com>
2022-11-01 16:30:12 -0700

    Javascript and HTML for function square

---
+++ lesson04/work.js
@@ -0,0 +1,12 @@
+function numberSquare() {
+  let input = prompt('Enter a number');
+  let numberToSquare = input;
+  let result = 0;
+
+
+  result = numberToSquare * Number(input);
+
+  alert(result);
+
+
+}
